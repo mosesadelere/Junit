e390d43c0c2c239a12b394789f8c97db2c876fbb
moses Adelere <moses.adelere@gmail.com>
2024-10-15 16:58:39 +0300

    test case with junit

--- src/main/java/org/samplejunit/StringUtils.java
+++ src/main/java/org/samplejunit/StringUtils.java
@@ -15,4 +15,33 @@ public class StringUtils {
         String reversed = reverse(input);
         return input.equals(reversed);
     }
+
+    public static Double convertToDouble(String str){
+        if (str == null){
+            return null;
+        }
+        return Double.valueOf(str);
+    }
+
+    public static boolean isNullorBlank(String str){
+        return  str == null || str.trim().isEmpty();
+    }
+
+    public static String getDefaultIfNull(final String str, final String defaultStr){
+        return str == null ? defaultStr : str;
+    }
+
+    public static String concat(String... sts){
+        String retVal = null;
+        if(sts != null && sts.length > 0){
+            StringBuilder sb = new StringBuilder();
+            for (String st : sts){
+                if (st != null){
+                    sb.append(st);
+                }
+            }
+            retVal = sb.toString();
+        }
+        return retVal;
+    }
 }
--- src/test/java/org/samplejunit/StringUtilsTest.java
+++ src/test/java/org/samplejunit/StringUtilsTest.java
@@ -1,8 +1,10 @@
 package org.samplejunit;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.samplejunit.StringUtils.*;
 
 class StringUtilsTest {
 
@@ -28,4 +30,102 @@ class StringUtilsTest {
     void testIsPalindromeWithNull(){
         assertThrows(IllegalAccessError.class, () -> stringUtils.isPalindrome(null));
     }
+
+    @Test
+    void testConvertToDoubleOK(){
+        String age = "2022";
+        Double expAge = Double.valueOf(age);
+        Double actual = convertToDouble(age);
+
+        assertAll("Do many assertions.", () -> {
+            assertNotNull(actual);
+            assertEquals(expAge, actual);
+        });
+
+        // sample lambda
+        assertAll("Do many assertions.Java 8 Lambda style", () -> {
+            assertNotNull(actual, () -> "This actual is Null.");
+            assertEquals(expAge, actual, () -> "The expected is: " + expAge + " while the actual is: " + actual);
+        });
+    }
+
+    @Test
+    void testConvertToDoubleWithNullArgument(){
+        String age = null;
+        Double actual = StringUtils.convertToDouble(age);
+        assertNull(actual, "The actual is not null");
+
+        assertNull(actual, () -> "The actual is not Null, Java 8 style.");
+    }
+
+    @Test
+    void testConvertToDoubleThrowException(){
+        String age = "N/A";
+        assertThrows(NumberFormatException.class, () -> {
+            StringUtils.convertToDouble(age);
+        });
+    }
+
+    @Test
+    void testIsNullOrBlankOK(){
+        String input = null;
+
+        assertTrue(StringUtils.isNullorBlank(input), () -> "This string is not null or blank");
+
+        input = " ";
+        assertTrue(StringUtils.isNullorBlank(input));
+
+        input = "abc";
+        assertFalse(StringUtils.isNullorBlank(input));
+    }
+
+    @Test
+    void testGetDefaultIfNull(){
+        String str = null;
+        String defaultStr = "abc";
+
+        String actual = StringUtils.getDefaultIfNull(str, defaultStr);
+        assertSame(defaultStr, actual, () -> "Expected output is not same with actual");
+
+        // test case with input string is not null
+        str = "def";
+        actual = StringUtils.getDefaultIfNull(str, defaultStr);
+        assertNotSame(defaultStr, actual, () -> "Expected output is same with actual");
+
+        // test case with input string is empty
+        str = "";
+        actual = StringUtils.getDefaultIfNull(str, defaultStr);
+        if (actual.equals(defaultStr)){
+            fail(() -> "The actual should be empty");
+        }
+    }
+
+    @Test
+    void testConcatWithRegularInput(){
+        String str1 = "Hello";
+        String str2 = "Naija";
+        String str3 = "!";
+        String expect = str1 + str2 +str3;
+        String actual = StringUtils.concat(str1, str2, str3);
+        assertEquals(expect, actual);
+    }
+
+    @Test
+    void testConcatWithNullInput(){
+        String str1 = "Hello";
+        String str2 = "Naija";
+        String str3 = null;
+        String expect = str1 + str2;
+        String actual = StringUtils.concat(str1, str2, str3);
+        assertEquals(expect, actual);
+    }
+
+    @Disabled
+    @Test
+    void testConcatWithAllNullInput(){
+        String actual = StringUtils.concat();
+        assertNull(actual);
+    }
+
+
 }
